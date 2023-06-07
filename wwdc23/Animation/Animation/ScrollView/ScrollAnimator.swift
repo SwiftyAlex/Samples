3519adea787c9518ada8eb21c41b06ad3d216653
Alex Logan <me@alexanderlogan.co.uk>
2023-06-07 14:13:28 +0100

    Add scroll view demo

---
+++ wwdc23/Animation/Animation/ScrollView/ScrollAnimator.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollAnimator.swift
+//  Animation
+//
+//  Created by Alex Logan on 07/06/2023.
+//
+
+import SwiftUI
+
+struct ScrollAnimator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollAnimator()
+}
